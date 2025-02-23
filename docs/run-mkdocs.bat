adb25460e43ed27abce0a554c735ce60b13e92cc
Aetherinox <aetherinox@proton.me>
2025-02-23 13:41:21 -0700

    ci: update mkdocs windows script to accept github access token on launch

--- docs/run-mkdocs.bat
+++ docs/run-mkdocs.bat
@@ -1,1 +1,57 @@
+@CD 	    /d "%~dp0"
+@ECHO 	    OFF
+TITLE       mkdocs startup
+SETLOCAL 	enableextensions enabledelayedexpansion
+MODE        con:cols=125 lines=120
+MODE        125,40
+GOTO        comment_end
+
+Starts up mkdocs from a windows system.
+Ensure you have defined `GH_TOKEN` or the git-committers plugin will rate limit you.
+
+    setx /m GH_TOKEN "github_pat_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+If using a Github Workflow, create a new secret in the repo settings named `GH_TOKEN`
+and give it your Github fine-grained personal access token.
+
+The token variable is defined in mkdocs.yml
+
+:comment_end
+
+ECHO.
+
+:: #
+::  define:     directories
+:: #
+
+set dir_home=%~dp0
+
+:: #
+::  define:     env variable
+:: #
+
+set TOKEN=%GH_TOKEN2%
+
+echo  --------------------------------------------------------------------------------
+echo    Mkdocs Launcher
+echo  --------------------------------------------------------------------------------
+
+IF [!TOKEN!]==[]  (
+    echo  --------------------------------------------------------------------------------
+    echo    GH_TOKEN not defined. Open %0%
+    echo    Create a new one at https://github.com/settings/personal-access-tokens
+    echo  --------------------------------------------------------------------------------
+    set /P TOKEN= Enter Github Personal Access Token (fine-grained):
+
+)
+
+echo    GH_TOKEN: !TOKEN!
+echo.
+echo.
+
+:: #
+::  start mkdocs
+:: #
+
+echo Starting mkdocs ...
 start cmd /k "mkdocs serve --clean"
